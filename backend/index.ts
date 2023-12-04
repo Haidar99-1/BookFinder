a4642c2311c0521dcbf220ca7f9244b1b1afb2a5
Haidar99-1 <56528909+Haidar99-1@users.noreply.github.com>
2023-12-04 14:31:12 -0500

    removed api_key variable

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 
-const api_key = 'OowQn7i01lyJLVIdaqXP1b0OkVKRT5kO';
+
 
 app.get(`https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=${api_key}`, (req: Request, res: Response) => {
   console.log(res.json());
